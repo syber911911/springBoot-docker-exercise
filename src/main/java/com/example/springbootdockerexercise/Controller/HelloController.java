babc995baf1bb9abcdb5673f23d637f453fe384b
이희준 <syber911911@naver.com>
2023-05-16 15:27:22 +0900

    feat: currentTime 추가

--- src/main/java/com/example/springbootdockerexercise/Controller/HelloController.java
+++ src/main/java/com/example/springbootdockerexercise/Controller/HelloController.java
@@ -1,11 +1,28 @@
 package com.example.springbootdockerexercise.Controller;
 
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDateTime;
 @RestController
 public class HelloController {
+    @GetMapping("/")
+    public void currentTime() {
+        String webhookUrl = "https://discord.com/api/webhooks/1090549186040184913/_nVKm8TfNkMjjZDz2lgNqbVQBmciLcgii6trO7nXIEz7LAtMHVRBXkSVwoSd-nYRGxzU";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        String payload = "{\"username\": \"HeeJun\", \"content\": \"" + LocalDateTime.now() + "\"}";
+        HttpEntity<String> entity = new HttpEntity<>(payload, headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.exchange(webhookUrl, HttpMethod.POST, entity, String.class);
+    }
+
     @GetMapping("/hello")
     public String helloPage() {
         return "Hello";
