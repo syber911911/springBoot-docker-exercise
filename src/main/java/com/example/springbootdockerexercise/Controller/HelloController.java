e914c773313e916e562ef49083f3637301db4841
이희준 <syber911911@naver.com>
2023-05-16 16:13:30 +0900

    fix: HelloController 수정

--- src/main/java/com/example/springbootdockerexercise/Controller/HelloController.java
+++ src/main/java/com/example/springbootdockerexercise/Controller/HelloController.java
@@ -1,28 +1,35 @@
 package com.example.springbootdockerexercise.Controller;
 
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
 @RestController
 public class HelloController {
-    @PostMapping("/webhook")
-    public String currentTime() {
+    @GetMapping("/")
+    public String getMyMemberInfo() {
+        return getString(LocalDateTime.now().toString());
+        // return "hello";
+    }
+
+    private static String getString(String string) {
+        // 웹훅 URL
         String webhookUrl = "https://discord.com/api/webhooks/1090549186040184913/_nVKm8TfNkMjjZDz2lgNqbVQBmciLcgii6trO7nXIEz7LAtMHVRBXkSVwoSd-nYRGxzU";
 
+        // 요청 헤더 설정
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        String payload = "{\"username\": \"HeeJun\", \"content\": \"" + LocalDateTime.now() + "\"}";
+        // 요청 바디 설정
+        String payload = "{\"username\": \"syber911911\", \"content\": \"" + string + "\"}";
         HttpEntity<String> entity = new HttpEntity<>(payload, headers);
 
+        // RestTemplate을 사용하여 POST 요청 보내기
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response = restTemplate.exchange(webhookUrl, HttpMethod.POST, entity, String.class);
 
+        // 응답 처리
         if (response.getStatusCode().is2xxSuccessful()) {
             return "Webhook sent successfully";
         } else {
@@ -30,8 +37,8 @@ public class HelloController {
         }
     }
 
-    @GetMapping("/hello")
-    public String helloPage() {
-        return "Hello";
+    @GetMapping("/hook/{string}")
+    public String sendHookWithString(@PathVariable("string") String string) {
+        return getString(string);
     }
 }
