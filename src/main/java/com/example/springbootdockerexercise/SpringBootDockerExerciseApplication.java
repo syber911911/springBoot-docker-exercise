369c14e424caf620247ac11fcdcf5eac052416eb
이희준 <syber911911@naver.com>
2023-05-16 14:09:38 +0900

    feat: project build

---
+++ src/main/java/com/example/springbootdockerexercise/SpringBootDockerExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockerexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootdockerexercise/SpringBootDockerExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockerexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
